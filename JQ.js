588fb7e72fef5d87414a76c1f11fcb48fa9d2e82
mcwong644 <mcwong644@gmail.com>
2023-08-03 19:27:41 +0800

    first commit

---
+++ JQ.js
@@ -0,0 +1,6 @@
+//https://www.youtube.com/live/Rkvn_MA04fo?feature=share
+//jQuery Full Course 2022 | jQuery Tutorial For Beginners | jQuery Tutorial | Simplilearn
+//stop 13:04
+function func1(){
+    $("#img1").fadeToggle(3000);
+}
